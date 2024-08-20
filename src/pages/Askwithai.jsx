116eb4bfe7b32fe0c873b58250ba4aa893f1a279
Prateek Agarwal <agarwalprateek666@gmail.com>
2024-08-20 12:53:41 +0530

    initial commit

---
+++ src/pages/Askwithai.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Aii from "../components/Aii";
+// import PromptComponent from "../components/Aii";
+
+// import PromptComponent from "../components/Aii";
+
+const AI = () => {
+  return (
+    <div>
+      <div>
+        {/* <PromptComponent className="centered-component" /> */}
+        <Aii className="centered-component" />
+      </div>
+    </div>
+  );
+};
+
+export default AI;
